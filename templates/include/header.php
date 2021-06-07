993212f3345db83e848aa020485b566a8157dfee
Giorox <giovannireboucas@hotmail.com>
2021-06-07 13:57:07 -0300

    Updated header and footer

--- templates/include/header.php
+++ templates/include/header.php
@@ -29,12 +29,27 @@
 
     <link rel="stylesheet" href="css/style.css">
 	
+	<!-- jQuery -->
+	<script src="js/jquery-3.3.1.min.js"></script>
+	<script src="js/jquery-migrate-3.0.1.min.js"></script>
+	<script src="js/jquery-ui.js"></script>
+	<script src="js/jquery.stellar.min.js"></script>
+	<script src="js/jquery.countdown.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	
+	<script src="js/bootstrap-datepicker.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/mediaelement-and-player.min.js"></script>
+	
+	
 
 	<!-- CKEditor -->
 	<script src="./js/ckeditor/ckeditor.js"></script>
 	
 	<!-- Scripts -->
 	<script src="https://unpkg.com/popper.js"></script>
+	<script src="js/popper.min.js"></script>
 	<script src="https://unpkg.com/tooltip.js"></script>
 	
 	<!-- Google Analytics Tag -->
