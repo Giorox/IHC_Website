ed95166a5cfcaf42192ee6b77aca04913cb93ac7
Giorox <giovannireboucas@hotmail.com>
2021-06-20 12:57:52 -0300

    Updated lastInsertID for user messages

--- registerUserContact.php
+++ registerUserContact.php
@@ -6,13 +6,13 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-// Insert the User
+// Insert the message
 $conn = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $sql = "INSERT INTO usermessages ( messageID, name, email, subject, message ) VALUES ( :messageID, :name, :email, :subject, :message )";
 
 $st = $conn->prepare ( $sql );
-$st->bindValue( ":messageID", $conn->lastInsertId(), PDO::PARAM_INT );
+$st->bindValue( ":messageID", $conn->lastInsertId("usermessages_messageID_seq"), PDO::PARAM_INT );
 $st->bindValue( ":name", $name, PDO::PARAM_STR );
 $st->bindValue( ":email", $email, PDO::PARAM_STR );
 $st->bindValue( ":subject", $subject, PDO::PARAM_STR );
